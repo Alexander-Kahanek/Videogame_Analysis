ed4c47a06d940af93a947c79c2e2384651bf4aff
Alexander-Kahanek <59942009+Alexander-Kahanek@users.noreply.github.com>
2020-12-09 16:30:34 -0600

    reformatted

--- manip.py
+++ manip.py
@@ -1,23 +1,21 @@
 import pandas as pd
 
 raw = pd.read_csv('vgsales.csv')
-# print(raw.head())
+
 years = raw.groupby(['Year']).agg({'NA_Sales': 'sum', 'EU_Sales': 'sum',
                                    'JP_Sales': 'sum', 'Other_Sales': 'sum', 'Global_Sales': 'sum'}).reset_index()
 
 plats = raw.groupby(['Year', 'Platform']).agg({'NA_Sales': 'sum', 'EU_Sales': 'sum',
                                                'JP_Sales': 'sum', 'Other_Sales': 'sum', 'Global_Sales': 'sum'}).reset_index()
 
-# print(pub.head())
-
 top_plats = []
 
 for year, df in plats.groupby(['Year']):
     df.index = df['Platform']
-    # NA = df.iloc[df['NA_Sales'].idxmax()]['Platform']
+
     NA, JP, EU, O, G = df[['NA_Sales', 'JP_Sales',
                            'EU_Sales', 'Other_Sales', 'Global_Sales']].idxmax()
-    # print(year, NA, JP, EU, O,  G)
+
     top_plats.append({
         "Year": year,
         "NA": NA,
@@ -28,16 +26,9 @@ for year, df in plats.groupby(['Year']):
     })
 
 plats = pd.DataFrame(top_plats)
-comb = years.merge(plats, on='Year', how='right')
+combined = years.merge(plats, on='Year', how='right')
 
-print(comb.head())
-# print(years.head(), '\n', plats.head())
+print('dataframes found and combined, printing head and saving new csv.')
+print(combined.head())
 
-comb.to_csv('year.sales.platforms.csv', index=False)
-
-# print(comb['NA'].unique())
-# print(comb['JP'].unique())
-# print(comb['EU'].unique())
-# print(comb['Other'].unique())
-# print(comb['Global'].unique())
-# print(raw['Platform'].unique())
+combined.to_csv('year.sales.platforms.csv', index=False)
